c1156ed537b1abf38a9d83afed109d6047dd1b75
放浪 軍師 <roamschemer@gmail.com>
2019-08-21 23:51:34 +0900

    プロジェクト ファイルを追加します。

---
+++ FFImageLoadingTest/FFImageLoadingTest.Android/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Prism;
+using Prism.Ioc;
+
+namespace FFImageLoadingTest.Droid
+{
+    [Activity(Label = "FFImageLoadingTest", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+protected override void OnCreate(Bundle bundle)
+{
+    TabLayoutResource = Resource.Layout.Tabbar;
+    ToolbarResource = Resource.Layout.Toolbar;
+
+    base.OnCreate(bundle);
+
+    global::Xamarin.Forms.Forms.Init(this, bundle);
+    FFImageLoading.Forms.Platform.CachedImageRenderer.Init(enableFastRenderer:true);  //ここ
+    LoadApplication(new App(new AndroidInitializer()));
+}
+    }
+
+    public class AndroidInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // Register any platform specific implementations
+        }
+    }
+}
+
